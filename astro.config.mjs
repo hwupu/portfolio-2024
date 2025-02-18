0dd443477df9a9a652c5cab76d838209407a0ada
Pu-Hsuan Wu <hwupu.cs01@nctu.edu.tw>
2025-02-18 23:29:11 +0800

    Experimental with online CV

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,30 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import { shield } from '@kindspells/astro-shield'
+
+import { resolve } from 'node:path'
+const rootDir = new URL('.', import.meta.url).pathname
+const modulePath = resolve(rootDir, 'src', 'generated', 'sriHashes.mjs')
 
 // https://astro.build/config
 export default defineConfig({
+  devToolbar: {
+    enabled: false
+  },
   vite: {
     plugins: [tailwindcss()],
   },
+  integrations: [
+    shield({
+      sri: { hashesModule: modulePath },
+      securityHeaders:{
+        contentSecurityPolicy:{
+          cspDirectives: {
+            'default-src': "'self'",
+          }
+        }
+      }
+    })
+  ]
 });
